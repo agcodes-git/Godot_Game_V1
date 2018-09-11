3578e895da71121582c5949f3d796f02afae1ef3
FeralBytes <feralbytes@gmail.com>
2018-09-11 10:10:00 -0800

    Boomerang Use and Ignore mono vs (#12)
    * Added simple boomerang teleport mechanic.
    
    * Changed the timer to a one_shot and removed the code to stop the restarted timer.
    
    * Added .mono and .vs to the ignore to support future C# code.

--- godot/scenes/player/Motion.gd
+++ godot/scenes/player/Motion.gd
@@ -3,22 +3,22 @@ extends Node
 var motion = Vector2(0,0)
 const UP = Vector2(0,-1)
 var player = null
-		
+        
 func _physics_process(delta):
-	if self.player == null: self.player = get_parent()
+    if self.player == null: self.player = get_parent()
 
-	apply_gravity(delta)
-	update_input()
-	self.motion = self.player.move_and_slide( self.motion, UP )
-	
+    apply_gravity(delta)
+    update_input()
+    self.motion = self.player.move_and_slide( self.motion, UP )
+    
 func apply_gravity(delta): 
-	self.motion.y += 2500 * delta
+    self.motion.y += 2500 * delta
 
 func update_input():
-	if Input.is_action_pressed("ui_up") and self.player.is_on_floor():
-		motion.y = -1500
-		
-	if Input.is_action_pressed("ui_right"): motion.x = 500
-	elif Input.is_action_pressed("ui_left"): motion.x = -500
-	else: motion.x = 0
-	
+    if Input.is_action_pressed("ui_up") and self.player.is_on_floor():
+        motion.y = -1500
+        
+    if Input.is_action_pressed("ui_right"): motion.x = 500
+    elif Input.is_action_pressed("ui_left"): motion.x = -500
+    else: motion.x = 0
+    
