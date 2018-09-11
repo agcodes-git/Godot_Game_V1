c4dc65879b2155dc5cd7485344b7684f80a155fd
rikedyp <rikedyp@gmail.com>
2018-09-11 23:29:17 +0100

    initial shader set up (experimenting)

---
+++ godot/scenes/PaletteTestScene.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+# Design:
+	# Five (or whatever) colours which can be set (no-outline style)
+	# Colours can be set plus contrasting sketchlines / outlines
+	
+# TODO: How to make sure the combinations look good?
+	# Dictionary of colour palettes?
+	# Use maths in colour space to ensure beauty / contrast?
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
