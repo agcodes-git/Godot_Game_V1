648b8cc9588e8afaa7e67a0d80268397adbec2cb
agodot <zoldy.wf@gmail.com>
2018-09-12 20:28:44 -0600

    Per FeralByte's suggestion, made several values exported variables for testing.

--- godot/scenes/boomerang/boomerang.gd
+++ godot/scenes/boomerang/boomerang.gd
@@ -2,13 +2,17 @@ extends RigidBody2D
 
 var player = null
 
+export var initial_teleport_time = 0.5
+export var additional_teleport_time = 0.5
+
 func _ready():
 	self.contact_monitor = true
 	self.contacts_reported = 2
+	self.player.get_node("Timer").wait_time = initial_teleport_time
 
 # Add time (1/2 a second) before the forced teleport for each collision.
 func _on_Node2D_body_entered(body):
 	print(player.get_node("Timer").time_left)
-	player.get_node("Timer").wait_time = player.get_node("Timer").time_left + 0.5
+	player.get_node("Timer").wait_time = player.get_node("Timer").time_left + additional_teleport_time
 	player.get_node("Timer").stop()
 	player.get_node("Timer").start()
--- godot/scenes/player/Motion.gd
+++ godot/scenes/player/Motion.gd
@@ -3,7 +3,11 @@ extends Node
 var motion = Vector2(0,0)
 const UP = Vector2(0,-1)
 var player = null
-        
+
+export var gravity_accel = 6000
+export var jump_impulse = 2500
+export var movement_vel = 1000
+
 func _physics_process(delta):
     if self.player == null: self.player = get_parent()
 
@@ -12,13 +16,13 @@ func _physics_process(delta):
     self.motion = self.player.move_and_slide( self.motion, UP )
     
 func apply_gravity(delta): 
-    self.motion.y += 6000 * delta
+    self.motion.y += gravity_accel * delta
 
 func update_input():
     if Input.is_action_pressed("ui_up") and self.player.is_on_floor():
-        motion.y = -2500
+        motion.y = -jump_impulse
         
-    if Input.is_action_pressed("ui_right"): motion.x = 1000
-    elif Input.is_action_pressed("ui_left"): motion.x = -1000
+    if Input.is_action_pressed("ui_right"): motion.x = movement_vel
+    elif Input.is_action_pressed("ui_left"): motion.x = -movement_vel
     else: motion.x = 0
     
