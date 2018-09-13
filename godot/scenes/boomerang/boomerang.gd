9954164e42ae2f8c181c9df742206df8fcb03d6d
agodot <zoldy.wf@gmail.com>
2018-09-12 20:39:56 -0600

    Minor refactor, moved timer to be child of boomerang instead of player.

--- godot/scenes/boomerang/boomerang.gd
+++ godot/scenes/boomerang/boomerang.gd
@@ -8,11 +8,15 @@ export var additional_teleport_time = 0.5
 func _ready():
 	self.contact_monitor = true
 	self.contacts_reported = 2
-	self.player.get_node("Timer").wait_time = initial_teleport_time
+	get_node("Timer").wait_time = initial_teleport_time
+	get_node("Timer").start()
+	print("ready")
 
 # Add time (1/2 a second) before the forced teleport for each collision.
 func _on_Node2D_body_entered(body):
-	print(player.get_node("Timer").time_left)
-	player.get_node("Timer").wait_time = player.get_node("Timer").time_left + additional_teleport_time
-	player.get_node("Timer").stop()
-	player.get_node("Timer").start()
+	get_node("Timer").wait_time = get_node("Timer").time_left + additional_teleport_time
+	get_node("Timer").stop()
+	get_node("Timer").start()
+
+func _on_Timer_timeout():
+	player.swap_with_boomerang()
--- godot/scenes/player/player.gd
+++ godot/scenes/player/player.gd
@@ -4,6 +4,8 @@ const boomerang_scene = preload("res://scenes/boomerang/boomerang.tscn")
 var mouse_down = false
 var boo = null
 
+export var boomerang_velocity = 3000
+
 # For controlling the boomerang.
 func _input(event):
 	if event.is_pressed() and event is InputEventMouseButton and event.button_index == (BUTTON_LEFT):
@@ -15,19 +17,13 @@ func _input(event):
 			var mouse_position = get_local_mouse_position()
 			boo.global_position = global_position
 			var direction = (mouse_position).normalized()
-			boo.linear_velocity = direction * 3000
-			get_node("Timer").start()
+			boo.linear_velocity = direction * boomerang_velocity
+			
 		else: # Allow the player to swap at any time by clicking.
 			swap_with_boomerang()
-
-# If there's no time left in the timer,
-# then force the player to teleport.
-func _on_Timer_timeout():
-	swap_with_boomerang()
 	
 func swap_with_boomerang():
 	if boo != null:
-		get_node("Timer").wait_time = 0.5
 		global_position = boo.global_position
 		boo.queue_free()
 		boo = null
